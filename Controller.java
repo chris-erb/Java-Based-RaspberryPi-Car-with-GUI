d6c1f50dafefaa6af15e798e5341fee28c4d3473
chris-erb <145140858+chris-erb@users.noreply.github.com>
2024-08-20 10:43:30 -0400

    Main Controller.java file
    This file must be run through the RaspberryPi command prompt using Pi4J 1.4

--- Controller.java
+++ Controller.java
@@ -83,7 +83,7 @@ public class Controller {
     }
 
     public static void main(String[] args) {
-		// Create a GUI object and a Controller object (utilizing GUI object)
+	// Create a GUI object and a Controller object (utilizing GUI object)
         GUI gui = new GUI();
         Controller controller = new Controller(gui);
 
